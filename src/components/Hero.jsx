650129c7f3e9a9a481e5ead60202db87be2f050e
Satyam kumar <satyamkumar2716@gmail.com>
2024-07-01 21:16:32 +0530

    change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,6 +44,9 @@ const Hero = () => {
               <a href="#contact" className="py-2 px-4" onClick={closeMenu}>
                 Contact
               </a>
+              <a href="/admin/login" className="py-2 px-4" onClick={closeMenu}>
+                Admin
+              </a>
             </nav>
             <button
               onClick={toggleMenu}
