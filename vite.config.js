cb4d8ea08d472ff47f0d0db2b639a4aec38c4c1a
Satyam kumar <satyamkumar2716@gmail.com>
2025-03-18 13:28:14 +0530

    change

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Vite Configuration
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure this matches your project's build output directory
-  },
-  server: {
-    port: 3000,
-  },
+    plugins: [react()],
+    build: {
+        outDir: 'dist', // Ensure this matches your project's build output directory
+    },
+    server: {
+        port: 3000, // Optional: If you want to change the development server's port
+    },
 });
